95505ff7169b4283c71f39b431d26317be79b28d
vovad <vova.degtyarev.01@mail.ru>
2021-02-14 15:24:15 +0500

    add new oop things, lesson 4.

--- app/src/test/java/com/example/chiply/ExampleUnitTest.kt
+++ app/src/test/java/com/example/chiply/ExampleUnitTest.kt
@@ -2,49 +2,47 @@ package com.example.chiply
 
 import org.junit.Test
 
-import org.junit.Assert.*
-import kotlin.math.pow
-import kotlin.math.sqrt
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun printDiscount() {
-        println(formatPrice(10.0, "кг", 20))
-        calculateKvadratnoeUravnenie(1.0, 4.0, 3.0)
+
+        val laysChips = Product(80.0, 20)
+        val pringlsChips = Product(180.0, 10)
+
+        val pricePrinter: PricePrinter = CleanKotlinPricePrinter()
+
+        val list = listOf(laysChips, pringlsChips)
+        list.forEach {
+            pricePrinter.print(it.calcDiscountPrice())
+        }
+
     }
 }
 
-fun formatPrice(price: Double, measure: String = "шт", discountPercent: Int): String {
-
-    return if (discountPercent != 0) {
-        val discountPrice = price - (price * discountPercent / 100)
-        "$discountPrice/$measure(скидка $discountPercent%)"
-    } else {
-        "$price/$measure"
-    }
+class Product (
+        private val price: Double,
+        private val discountPercent: Int = 0
+) {
+    fun calcDiscountPrice(): Double = price - (price * discountPercent / 100)
 }
 
-fun calculateKvadratnoeUravnenie(a: Double = 0.0, b: Double = 0.0, c: Double = 0.0) {
+interface PricePrinter {
+    fun print(price: Double)
+}
 
-    val discriminant = b.pow(2.0) - 4 * a * c
-
-    when {
-        discriminant > 0.0 -> {
-            val x1 = (-b - sqrt(discriminant)) / (2 * a)
-            val x2 = (-b + sqrt(discriminant)) / (2 * a)
-            println("Первый корень - $x1. Второй корень - $x2")
-        }
-        discriminant == 0.0 -> {
-            val x1 = (-b) / (2 * a)
-            println("Один корень - $x1")
-        }
-        else -> {
-            println("Решений нет")
+class CleanKotlinPricePrinter : PricePrinter {
+    override fun print(price: Double) {
+        if (price > 100) {
+            println("Цена - $price руб. Дороговато чет")
+        } else {
+            println("Цена - $price руб. Норм тема")
         }
     }
+
 }
