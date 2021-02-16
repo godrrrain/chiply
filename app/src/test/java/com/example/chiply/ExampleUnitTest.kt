2cdb702d10d0cb91e1eaa91bd32a2a420be34389
vovad <vova.degtyarev.01@mail.ru>
2021-02-16 21:20:33 +0500

    add cart, lesson 5.

--- app/src/test/java/com/example/chiply/ExampleUnitTest.kt
+++ app/src/test/java/com/example/chiply/ExampleUnitTest.kt
@@ -12,37 +12,70 @@ class ExampleUnitTest {
     @Test
     fun printDiscount() {
 
-        val laysChips = Product(80.0, 20)
-        val pringlsChips = Product(180.0, 10)
+        val laysChips = Product("Лейс",80.0, 20)
+        val pringlsChips = Product("Принглз",180.0, 10)
 
         val pricePrinter: PricePrinter = CleanKotlinPricePrinter()
 
-        val list = listOf(laysChips, pringlsChips)
-        list.forEach {
-            pricePrinter.print(it.calcDiscountPrice())
+        val products = listOf(laysChips, pringlsChips)
+        val discountPrices = products.map { it.calcDiscountPrice() }
+
+        discountPrices.forEach { discountPrice ->
+            pricePrinter.printProduct(discountPrice)
         }
+
+        val mainCart = Cart(products)
+        pricePrinter.printCart(mainCart)
 
     }
 }
 
+class Cart (
+        private val products: List<Product>
+) {
+    fun getOverallDiscountPrice(): Double {
+        var sum = 0.0
+        products.forEach {
+            sum += it.calcDiscountPrice()
+        }
+        return sum
+    }
+    fun getProductList(): List<Product> {
+        return products
+    }
+}
+
 class Product (
+        private val name: String,
         private val price: Double,
         private val discountPercent: Int = 0
 ) {
     fun calcDiscountPrice(): Double = price - (price * discountPercent / 100)
+
+    fun getName(): String = name
 }
 
 interface PricePrinter {
-    fun print(price: Double)
+    fun printProduct(price: Double)
+
+    fun printCart(cart: Cart)
 }
 
 class CleanKotlinPricePrinter : PricePrinter {
-    override fun print(price: Double) {
+    override fun printProduct(price: Double) {
         if (price > 100) {
             println("Цена - $price руб. Дороговато чет")
         } else {
             println("Цена - $price руб. Норм тема")
         }
+    }
+
+    override fun printCart(cart: Cart) {
+        val products = cart.getProductList()
+        products.forEach {
+            println("${it.getName()} со скидкой стоит = ${it.calcDiscountPrice()} руб")
+        }
+        println("Суммарная стоимость корзины = ${cart.getOverallDiscountPrice()} руб")
     }
 
 }
